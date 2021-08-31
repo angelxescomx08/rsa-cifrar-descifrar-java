db86f96fd3042ad337a73b0adcb7c01f857bcb3b
Jose Angel Hernandez Radilla <jhernandezr1313@alumno.ipn.mx>
2021-08-31 11:42:17 -0500

    RSA llaves

---
+++ src/Practica0/Cifrar.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Practica0;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.security.InvalidKeyException;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Arrays;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
+/**
+/**
+ *
+ * @author Bryan
+ */
+public class Cifrar {
+    public PrivateKey PrivateKey;
+    public PublicKey PublicKey;
+    public String text;
+   // constructor
+
+    public void setPublicKeyString(String key) throws NoSuchAlgorithmException, InvalidKeySpecException{
+        
+        byte[] encodedPublicKey = stringToBytes(key);
+        
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        X509EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(encodedPublicKey);
+        PublicKey publicKey = keyFactory.generatePublic(publicKeySpec);
+        this.PublicKey = publicKey;
+    }
+    
+    public void setPrivateKeyString(String key) throws NoSuchAlgorithmException, InvalidKeySpecException{
+        
+        byte[] encodedPrivateKey = stringToBytes(key);
+        
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        PKCS8EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(encodedPrivateKey);
+        PrivateKey privateKey = keyFactory.generatePrivate(privateKeySpec);
+        this.PrivateKey = privateKey;
+    }
+    public String Cipher(String plain,String tipo) throws NoSuchAlgorithmException,NoSuchPaddingException, InvalidKeyException,IllegalBlockSizeException, BadPaddingException, InvalidKeySpecException, UnsupportedEncodingException, NoSuchProviderException {
+
+        byte[] encryptedBytes; 
+  
+        Cipher cipher = Cipher.getInstance("RSA");
+        if(tipo.equals("public")){
+            System.out.println("public");
+            cipher.init(Cipher.ENCRYPT_MODE, this.PublicKey);
+        }
+            
+        else{
+            System.out.println("private");
+            cipher.init(Cipher.ENCRYPT_MODE, this.PrivateKey);
+        }
+        encryptedBytes = cipher.doFinal(plain.getBytes());
+
+        return bytesToString(encryptedBytes);
+
+    }
+    
+    public void openFromDiskPublicKey(String path) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+        String content = this.readFileAsString(path);
+        this.setPublicKeyString(content);
+    }
+    
+    public void openFromDiskPrivateKey(String path) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+        String content = this.readFileAsString(path);
+        this.setPrivateKeyString(content);
+    }
+    
+    public String readFileAsString(String filePath) throws IOException {
+        StringBuffer fileData = new StringBuffer();
+        BufferedReader reader = new BufferedReader(
+                new FileReader(filePath));
+        char[] buf = new char[1024];
+        int numRead=0;
+        while((numRead=reader.read(buf)) != -1){
+            String readData = String.valueOf(buf, 0, numRead);
+            fileData.append(readData);
+        }
+        reader.close();
+        return fileData.toString();
+    }
+    
+     public byte[] stringToBytes(String s) {
+        byte[] b2 = new BigInteger(s, 36).toByteArray();
+        return Arrays.copyOfRange(b2, 1, b2.length);
+    }
+      public String bytesToString(byte[] b) {
+        byte[] b2 = new byte[b.length + 1];
+        b2[0] = 1;
+        System.arraycopy(b, 0, b2, 1, b.length);
+        return new BigInteger(b2).toString(36);
+    }
+
+    
+    
+}
